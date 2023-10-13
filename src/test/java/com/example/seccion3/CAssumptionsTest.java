dcb91db3bfb495334e9d9f20cf08f6ddd5f6c4e2
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2023-10-13 11:38:19 +0200

    Tema 3 Assumptions test

--- src/test/java/com/example/seccion3/CAssumptionsTest.java
+++ src/test/java/com/example/seccion3/CAssumptionsTest.java
@@ -1,6 +1,7 @@
 package com.example.seccion3;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assumptions.*;
@@ -8,37 +9,35 @@ import static org.junit.jupiter.api.Assumptions.*;
 
 public class CAssumptionsTest {
 
-
     @BeforeAll
-    static void beforeAll() {
+    static void setUp(){
         System.getenv().forEach(
-                (key, value) -> System.out.println(key + " - " + value)
+                (k,v)->System.out.println(k+" : "+v)
         );
 
         System.getProperties().forEach(
-                (key, value) -> System.out.println(key + " - " + value)
+                (k,v)->System.out.println(k+" : "+v)
         );
     }
 
     @Test
-    void name() {
+    @DisplayName("Comprueba el JDK")
+    void test1() {
         String jdk = System.getenv("JAVA_HOME");
-        assumeTrue(jdk.contains("jdk-11"));
-        assumeFalse(jdk.contains("jdk-16.0.2"));
+        assumeTrue(jdk.contains("jdk-20"));
+        assumeFalse(jdk.contains("jdk-11"));
 
         System.out.println("El test continua");
     }
 
     @Test
-    void name2() {
+    @DisplayName("Comprueba el JDK con assumingThat")
+    void test2() {
         String jdk = System.getenv("JAVA_HOME");
-
-        assumingThat(jdk.contains("jdk-11"),
-                () -> {
-
-
+        assumingThat(jdk.contains("jdk-20"),
+                ()-> {
+                    System.out.println("El test continua");
                 }
         );
-
     }
 }
