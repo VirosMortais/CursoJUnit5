94bdbeb16e0e78d22d3e8612f9c6013c44db14c4
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2023-10-13 11:35:46 +0200

    Tema 1 assertions test

--- src/test/java/com/example/seccion1/AAssertionsTest.java
+++ src/test/java/com/example/seccion1/AAssertionsTest.java
@@ -10,16 +10,14 @@ public class AAssertionsTest {
 
     @Test
     void count() {
-
         SmartPhoneService service = new SmartPhoneServiceImpl();
 
         Integer count = service.count();
 
-        // assertions
+        // Assertions
         assertNotNull(count);
         assertTrue(count > 0);
-        assertEquals(3, count);
-        // assertEquals(3, count + 1, "El numero de elementos esperado es 3");
-
+        assertEquals(3, count, "El numero de elementos esperado es 3");
+        
     }
 }
