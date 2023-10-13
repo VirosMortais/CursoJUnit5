f9da643035ce5f1e423c35fd6c0b99b3e4a8cf03
Charles Arruda Santos <charlestacodeouro@hotmail.com>
2023-10-13 11:38:02 +0200

    Tema 1 AssertAll test

--- src/test/java/com/example/seccion1/CAssertAllTest.java
+++ src/test/java/com/example/seccion1/CAssertAllTest.java
@@ -12,18 +12,16 @@ public class CAssertAllTest {
     @Test
     void assertAllTest() {
 
-        // Class Under Test
+        //Class under Test
         // System Under Test (SUT)
         SmartPhoneService service = new SmartPhoneServiceImpl();
 
         Integer count = service.count();
 
-        // aserciones
-
         assertAll("Comprobaciones",
                 () -> assertNotNull(count),
                 () -> assertTrue(count > 0),
                 () -> assertEquals(3, count)
-                );
+        );
     }
 }
